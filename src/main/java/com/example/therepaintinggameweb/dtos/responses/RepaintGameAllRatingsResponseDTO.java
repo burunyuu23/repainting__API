cdb9960268ca8c3d0bd9906a98437db37da58b34
danilkk <ezzfvkoko@gmail.com>
2023-07-22 01:27:15 +0300

    remove useless

--- src/main/java/com/example/therepaintinggameweb/dtos/responses/RepaintGameAllRatingsResponseDTO.java
+++
@@ -1,4 +0,0 @@
-package com.example.therepaintinggameweb.dtos.responses;
-
-public class RepaintGameAllRatingsResponseDTO {
-}
--- src/main/java/com/example/therepaintinggameweb/dtos/responses/UserGameStoryResponseDTO.java
+++
@@ -1,7 +0,0 @@
-package com.example.therepaintinggameweb.dtos.responses;
-
-import lombok.Data;
-
-@Data
-public class UserGameStoryResponseDTO {
-}
