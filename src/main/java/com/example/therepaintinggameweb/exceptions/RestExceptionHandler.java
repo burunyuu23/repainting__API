b1829a90f7aef348642fbb6c31cc1cac4b55eac4
danilkk <ezzfvkoko@gmail.com>
2023-08-04 23:19:09 +0300

    feat: add PyExceptions handler

--- src/main/java/com/example/therepaintinggameweb/exceptions/RestExceptionHandler.java
+++ src/main/java/com/example/therepaintinggameweb/exceptions/RestExceptionHandler.java
@@ -2,9 +2,12 @@ package com.example.therepaintinggameweb.exceptions;
 import com.example.therepaintinggameweb.dtos.responses.ErrorResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.python.core.PyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static org.python.core.Py.RuntimeError;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
@@ -16,4 +19,11 @@ public class RestExceptionHandler {
         return ResponseEntity.status(exception.getStatus())
                 .body(modelMapper.map(exception, ErrorResponseDTO.class));
     }
+
+    @ExceptionHandler(value = {PyException.class})
+    public ResponseEntity<ErrorResponseDTO> handlePyException(PyException exception) {
+        return ResponseEntity.status(500)
+                .body(modelMapper.map(exception, ErrorResponseDTO.class));
+    }
+
 }
